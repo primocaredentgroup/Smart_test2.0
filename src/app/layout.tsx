48cf1c0d29aaead81abc7837a205a9ade18b994c
Simone Petretto <simone@example.com>
2025-09-05 11:54:21 +0200

    fix: corretti import Auth0 SDK
    - Rimosso /client dagli import Auth0
    - Fix per errore webpack
    - Dovrebbe risolvere il build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { ConditionalLayout } from "@/components/ConditionalLayout";
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
